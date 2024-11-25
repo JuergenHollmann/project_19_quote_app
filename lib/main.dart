54becac127f982a0d919b6f8245b913866778c5b
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-11-25 20:52:41 +0100

    added TextWidget with quote and author, plus ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -12,41 +12,78 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Zitate-App'),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Käsebrot ist ein gutes Brot!',
+              style: TextStyle(
+                color: Color.fromARGB(255, 0, 82, 149),
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              textAlign: TextAlign.right,
+              'Helge Schneider',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text(
+                "Neues Zitat zeigen",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
 }
 
-/*
+/*--------------------------------------------------------------------------------------------*
 Projekt 19 - (5.4.1-5.4.5) - Zitate-App
-https://docs.google.com/document/d/1Sehd4haowgndEbZL3dCXvsQGg6xzVux3r8EBEvh0-sc/edit?tab=t.0
-
-Erstelle ein neues GitHub-Repository, das für dein Projekt gedacht ist.
-
+√ https://docs.google.com/document/d/1Sehd4haowgndEbZL3dCXvsQGg6xzVux3r8EBEvh0-sc/edit?tab=t.0
+*---------------------------------------------------------------------------------------------*
+√ Erstelle ein neues GitHub-Repository, das für dein Projekt gedacht ist.
+√ https://github.com/JuergenHollmann/project_19_quote_app
+*---------------------------------------------------------------------------------------------*
 √ API-Textdaten abrufen und anzeigen
 √ Nun sollst du Daten von einer bestimmten API abrufen.
 √ Erstelle dir einen Account bei API-Ninjas und erzeuge einen API-Key.
 √ Verwende Thunder oder etwas Ähnliches, um eine GET-Anfrage an die “Quotes API” zu senden.
 √ Die URL der API ist: https://api-ninjas.com/api/quotes 
 √ Führe die Anfrage durch und untersuche den JSON-Body, der das Zitat und den Autor enthält.
-
-Die Flutter-UI implementieren
-Erstelle eine Flutter-App, die mindestens ein Text-Widget für ein Zitat und einen Button enthält.
-Der Button muss noch nichts machen, sondern wird später ausgebaut.
-
-Erstelle ein Flutter-Projekt.
-Füge ein Text-Widget mit einem Beispiel-Zitat hinzu.
-Füge einen Button hinzu.
-Mache commit und push.
-
+*---------------------------------------------------------------------------------------------*
+√ Die Flutter-UI implementieren
+√ Erstelle eine Flutter-App, die mindestens ein Text-Widget für ein Zitat und einen Button enthält.
+√ Der Button muss noch nichts machen, sondern wird später ausgebaut.
+*---------------------------------------------------------------------------------------------*
+√ Erstelle ein Flutter-Projekt.
+√ Füge ein Text-Widget mit einem Beispiel-Zitat hinzu.
+√ Füge einen Button hinzu.
+√ Mache commit und push.
+*---------------------------------------------------------------------------------------------*
 Ein einfaches Textzitat anzeigen
 Nachdem die UI jetzt steht, kann das Abrufen von Daten eingebaut werden.
 Baue die App so um, dass sie bei einem Knopfdruck ein zufälliges Zitat von der “Quotes API” abruft und auf dem Bildschirm anzeigt.
 Verwende die http-Bibliothek, um eine GET-Anfrage zu stellen, und zeige das Zitat und den Autor in einem Text-Widget an.
 Anmerkung: Vergiss nicht, deinen API-Key einzutragen 🙂
-
+*---------------------------------------------------------------------------------------------*
 Letztes Zitat speichern
 Das letzte Zitat, das geholt wurde, soll nun gespeichert werden, damit beim App-Start gleich etwas angezeigt werden kann.
 Füge das “SharedPreferences” Package zu deiner App hinzu.
@@ -54,23 +91,23 @@ Sorge dafür, dass ein geholtes Zitat immer auch in den SharedPreferences gespei
 Beim App-Start soll immer ein Zitat aus den SharedPreferences geholt werden und dieses angezeigt werden.
 Hier musst du den Fall beachten, dass es noch kein gespeichertes Zitat gibt.
 Baue zudem einen Button ein, um das letzte Zitat zu löschen.
-
+*---------------------------------------------------------------------------------------------*
 BONUS-Aufgaben:
+*---------------------------------------------------------------------------------------------*
 Kategorie von Zitaten anzeigen lassen
 Füge ein Dropdown-Menü zu deiner Flutter-App hinzu, mit dem der Benutzer eine bestimmte Kategorie von Zitaten auswählen kann (z.B. “Inspirational”, “Love”).
 Die Zitate können auch von der API geholt werden. Dies muss beim App-Start passieren. Baue dies ein.
 Verändere die App so, dass Zitate basierend auf der Auswahl geladen werden.
 Baue einen Button für das Zurücksetzen der Zitat-Kategorie ein.
-
+*---------------------------------------------------------------------------------------------*
 Ausgewählte Zitate-Kategorie speichern
 Speichere auch die Zitate-Kategorie über den App-Start hinweg.
-
-
+*---------------------------------------------------------------------------------------------*
 Woher kommen die Daten?
 https://api-ninjas.com/profile
 X-Api-Key  erforderlich = mit dem Konto verknüpfter API-Schlüssel:
 API-Key: l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n
-
+*---------------------------------------------------------------------------------------------*
 var headersList = {
  'Accept': 'application/json',
  'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
@@ -90,7 +127,7 @@ if (res.statusCode >= 200 && res.statusCode < 300) {
 else {
   print(res.reasonPhrase);
 }
-
+*---------------------------------------------------------------------------------------------*
 Auswahl der Kategorien:
 age
 alone
@@ -159,4 +196,4 @@ money
 morning
 movies
 success
-*/
+*--------------------------------------------------------------------------------------------*/
