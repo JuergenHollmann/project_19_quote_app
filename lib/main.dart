ce920e1207ae6a5046f4b950f1892de1a9e8244d
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-11-26 01:21:23 +0100

    added new code

--- lib/main.dart
+++ lib/main.dart
@@ -60,52 +60,115 @@ class GetUriFromAPI extends StatelessWidget {
       style: ButtonStyle(
         backgroundColor: WidgetStateProperty.all(Colors.blue),
       ),
+
+      // onPressed: () async {
+
+      //   const String apiUrl =
+      //       'https://api.api-ninjas.com/v1/quotes?category=birthday';
+      //   const String apiKey = 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n';
+
+      //   /*--------------------------------- response-Beispiel von api-ninjas ---*/
+      //   // [
+      //   // {
+      //   // "quote": "I'm amazed. When I was 40, I thought I'd never make 50. (usw.)",
+      //   // "author": "Gloria Stuart",
+      //   // "category": "birthday"
+      //   // }
+      //   // ]
+      //   /*--------------------------------- response-Beispiel von api-ninjas - ENDE ---*/
+
+      //   // const response = await fetch(
+      //   //     'https://api.api-ninjas.com/v1/quotes?category=birthday' + city, {
+      //   //   headers: {'X-API-Key': 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n'}
+      //   // });
+      //   // const data = await response.json();
+
+      //   /*--------------------------------- *** ---*/
+      //   // Für die HTTP-Server-Anfrage wird das --> import "package:http/http.dart;" <-- verwendet.
+
+      //   // Der "uriString" ist der Link zum Server.
+      //   const uriString =
+      //       //     "https://api.api-ninjas.com/v1/quotes?category=birthday";
+
+      //       //, headers: {'X-API-Key': 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n'}"; // missing API-Key!
+      //       "https://dummyjson.com/products"; // nur zum Testen - funzt OHNE API-Key
+
+      //   // Die Anfrage an den Server wird mit "await get(Uri.parse(uriString));" gestellt.
+      //   // Als Antwort kommt die "reponse" zurück.
+      //   log("apiUrl: $apiUrl");
+      //   log("apiKey: $apiKey");
+      //   log("uriString: $uriString");
+
+      //   // final response = await get(Uri.parse('$apiUrl?key=$apiKey')); // deaktiviert
+      //   final response = await get(Uri.parse(uriString));
+
+      //   // (Uri.parse(
+      //   //     "$uriString?Key=l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n"));
+
+      //   log("0092 - response: ${response.body}");
+
+      //   /*--------------------------------- 541-25 ---*/
+      //   List<String> productTitles = [];
+      //   log("0078 - leere Liste: $productTitles");
+
+      //   // Die Daten sind im "response.body" als JSON-String, der umgewandelt werden muss.
+      //   // Dafür wird die Funktion "jsonDecode()"" verwendet.
+      //   final productData = json.decode(response.body);
+      //   log("0083 $productData");
+
+      //   // Der Rückgabewert ist entweder eine Map oder eine List oder eine verschachtelte Version davon (je nachdem).
+      //   final productsJsonList = productData["quote"];
+      //   //log("0087 $productsJsonList");
+
+      //   // Durch die zurückgegebene Liste iterieren.
+      //   for (final product in productsJsonList) {
+      //     // In jedem Durchgang wird das JSON-Ergebnis weiter aufgebaut.
+      //     productTitles.add(product["quote"]);
+      //     log("0093 $productTitles");
+      //     /*--------------------------------- *** ---*/
+      //   }
+
+      //   log("--------------------------------------------------------------------------------------------------------");
+      //   // log("0097 - uriString:        $uriString");
+      //   log("--------------------------------------------------------------------------------------------------------");
+      //   /*--------------------------------- *** ---*/
+
+      // /*--------------------------------- Code von ThunderClient ---*/
+      // var headersList = {
+      //   'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+      //   'Accept': 'application/json',
+      //   'X-Api-Key': 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n'
+      // };
+      // var url =
+      //     Uri.parse('https://api.api-ninjas.com/v1/quotes?category=birthday');
+
+      // var req = http.Request('GET', url); // Original
+      //         //var req = ???.???('GET', url); // funzt nicht!
+
+      // req.headers.addAll(headersList);
+
+      // var res = await req.send();
+      // final resBody = await res.stream.bytesToString();
+
+      // if (res.statusCode >= 200 && res.statusCode < 300) {
+      //   log(resBody);
+      // } else {
+      //   log(res.reasonPhrase);
+      // }
+      // /*--------------------------------- Code von ThunderClient - ENDE ---*/
+
+      /*--------------------------------- 541-24 ---*/
       onPressed: () async {
-        const String apiUrl =
-            'https://api.api-ninjas.com/v1/quotes?category=birthday';
-        const String apiKey = 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n';
-
-// Future<http.Response> fetchData() async {
-//   final url = Uri.parse('$apiUrl?key=$apiKey');
-//   final response = await http.get(url);
-//   return response;
-
-        /*--------------------------------- response-Beispiel von api-ninjas ---*/
-        // [
-        // {
-        // "quote": "I'm amazed. When I was 40, I thought I'd never make 50. (usw.)",
-        // "author": "Gloria Stuart",
-        // "category": "birthday"
-        // }
-        // ]
-        /*--------------------------------- response-Beispiel von api-ninjas - ENDE ---*/
-
-        // const response = await fetch(
-        //     'https://api.api-ninjas.com/v1/quotes?category=birthday' + city, {
-        //   headers: {'X-API-Key': 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n'}
-        // });
-        // const data = await response.json();
-
-        /*--------------------------------- *** ---*/
         // Für die HTTP-Server-Anfrage wird das --> import "package:http/http.dart;" <-- verwendet.
 
         // Der "uriString" ist der Link zum Server.
-        // const uriString =
-        //     "https://api.api-ninjas.com/v1/quotes?category=birthday";
-
-        //, headers: {'X-API-Key': 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n'}"; // missing API-Key!
-        //"https://dummyjson.com/products"; // nur zum Testen - funzt OHNE API-Key
+        const uriString =
+            //"https://api.api-ninjas.com/v1/quotes?category=birthday"; // missing API-Key!
+            "https://dummyjson.com/products"; // nur zum Testen - funzt OHNE API-Key
 
         // Die Anfrage an den Server wird mit "await get(Uri.parse(uriString));" gestellt.
         // Als Antwort kommt die "reponse" zurück.
-        log("apiUrl: $apiUrl");
-        log("apiKey: $apiKey");
-
-        final response = await get(Uri.parse('$apiUrl?key=$apiKey'));
-        // (Uri.parse(
-        //     "$uriString?Key=l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n"));
-
-        log("0092 - response: ${response.body}");
+        final response = await get(Uri.parse(uriString));
 
         /*--------------------------------- 541-25 ---*/
         List<String> productTitles = [];
@@ -117,93 +180,29 @@ class GetUriFromAPI extends StatelessWidget {
         log("0083 $productData");
 
         // Der Rückgabewert ist entweder eine Map oder eine List oder eine verschachtelte Version davon (je nachdem).
-        final productsJsonList = productData["quote"];
+        final productsJsonList = productData['products'];
         //log("0087 $productsJsonList");
 
         // Durch die zurückgegebene Liste iterieren.
         for (final product in productsJsonList) {
           // In jedem Durchgang wird das JSON-Ergebnis weiter aufgebaut.
-          productTitles.add(product["quote"]);
+          productTitles.add(product["title"]);
           log("0093 $productTitles");
           /*--------------------------------- *** ---*/
         }
-
         log("--------------------------------------------------------------------------------------------------------");
-        // log("0097 - uriString:        $uriString");
+        log("0097 - uriString:        $uriString");
         log("--------------------------------------------------------------------------------------------------------");
-        /*--------------------------------- *** ---*/
-
-        // /*--------------------------------- Code von ThunderClient ---*/
-        // var headersList = {
-        //   'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-        //   'Accept': 'application/json',
-        //   'X-Api-Key': 'l+CE1RR3ZW0yIo6dhwc0HQ==aY0laKm7ph0Yud7n'
-        // };
-        // var url =
-        //     Uri.parse('https://api.api-ninjas.com/v1/quotes?category=birthday');
-
-        // var req = http.Request('GET', url); // Original
-        //         //var req = ???.???('GET', url); // funzt nicht!
-
-        // req.headers.addAll(headersList);
-
-        // var res = await req.send();
-        // final resBody = await res.stream.bytesToString();
-
-        // if (res.statusCode >= 200 && res.statusCode < 300) {
-        //   log(resBody);
-        // } else {
-        //   log(res.reasonPhrase);
-        // }
-        // /*--------------------------------- Code von ThunderClient - ENDE ---*/
-
-        // /*--------------------------------- 541-24 ---*/
-        // onPressed: () async {
-        //   // Für die HTTP-Server-Anfrage wird das --> import "package:http/http.dart;" <-- verwendet.
-
-        //   // Der "uriString" ist der Link zum Server.
-        //   const uriString =
-        //       "https://api.api-ninjas.com/v1/quotes?category=birthday"; // missing API-Key!
-        //   //"https://dummyjson.com/products"; // nur zum Testen - funzt OHNE API-Key
-
-        //   // Die Anfrage an den Server wird mit "await get(Uri.parse(uriString));" gestellt.
-        //   // Als Antwort kommt die "reponse" zurück.
-        //   final response = await get(Uri.parse(uriString));
-
-        //   /*--------------------------------- 541-25 ---*/
-        //   List<String> productTitles = [];
-        //   log("0078 - leere Liste: $productTitles");
-
-        //   // Die Daten sind im "response.body" als JSON-String, der umgewandelt werden muss.
-        //   // Dafür wird die Funktion "jsonDecode()"" verwendet.
-        //   final productData = json.decode(response.body);
-        //   log("0083 $productData");
-
-        //   // Der Rückgabewert ist entweder eine Map oder eine List oder eine verschachtelte Version davon (je nachdem).
-        //   final productsJsonList = productData['products'];
-        //   //log("0087 $productsJsonList");
-
-        //   // Durch die zurückgegebene Liste iterieren.
-        //   for (final product in productsJsonList) {
-        //     // In jedem Durchgang wird das JSON-Ergebnis weiter aufgebaut.
-        //     productTitles.add(product["title"]);
-        //     log("0093 $productTitles");
-        //     /*--------------------------------- *** ---*/
-        //   }
-        //   log("--------------------------------------------------------------------------------------------------------");
-        //   log("0097 - uriString:        $uriString");
-        //   log("--------------------------------------------------------------------------------------------------------");
-        //   log("0098 - response:         $response");
-        //   log("--------------------------------------------------------------------------------------------------------");
-        //   log("0099 - productTitles:    $productTitles");
-        //   log("--------------------------------------------------------------------------------------------------------");
-        //   log("0100 - productData:      $productData");
-        //   log("--------------------------------------------------------------------------------------------------------");
-        //   log("0101 - productsJsonList: $productsJsonList");
-        //   log("--------------------------------------------------------------------------------------------------------");
-        // },
-        // /*--------------------------------- *** ---*/
+        log("0098 - response:         ${response.body}");
+        log("--------------------------------------------------------------------------------------------------------");
+        log("0099 - productTitles:    $productTitles");
+        log("--------------------------------------------------------------------------------------------------------");
+        log("0100 - productData:      $productData");
+        log("--------------------------------------------------------------------------------------------------------");
+        log("0101 - productsJsonList: $productsJsonList");
+        log("--------------------------------------------------------------------------------------------------------");
       },
+      /*--------------------------------- *** ---*/
       child: const Text(
         "Neues Zitat zeigen",
         style: TextStyle(
